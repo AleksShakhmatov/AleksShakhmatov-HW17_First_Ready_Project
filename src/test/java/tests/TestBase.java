71138dcc44f8eb1e49eeb592c7d16d5d9ae22a65
AShakhmatov <Coluchy@yandex.ru>
2024-03-14 22:30:34 +0600

    add README.md

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,15 +13,14 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        System.setProperty("environment", System.getProperty("environment", "prod"));
 
         Configuration.baseUrl = "https://ibs.ru/";
-        Configuration.pageLoadStrategy = "normal";
+        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("browserRemoteUrl");
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
